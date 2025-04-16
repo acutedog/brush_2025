cb03b7e49e8e404952e0327591975c33750865ec
OldTrace <liyedadi@gmail.com>
2025-04-16 22:46:18 +0800

    完善ListNode方法，方便调试

--- src/ListNode.java
+++ src/ListNode.java
@@ -11,20 +11,49 @@ public class ListNode {
         this.val = val;
     }
 
+    ListNode(int[] nums) {
+        ListNode dummy = new ListNode(), head = dummy;
+        for (int num : nums) {
+            head.next = new ListNode(num);
+            head = head.next;
+        }
+        this.val = nums[0];
+        this.next = dummy.next.next;
+    }
+
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
 
-    void reverse() {
-
+    ListNode reverse() {
+        if (next == null) {
+            return this;
+        }
+        ListNode pre = null, cur = this;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
     }
 
     void print() {
-
+        ListNode cur = this;
+        StringBuilder sb = new StringBuilder();
+        while (cur != null) {
+            sb.append(cur.val).append(" ");
+            cur = cur.next;
+        }
+        System.out.println(sb.toString().trim());
     }
 
-    ListNode convertArrayToListNode(int[] array) {
-        return null;
+    public static void main(String[] args) {
+        ListNode nums = new ListNode(new int[] {2,4,3});
+        nums.print();
+        ListNode reverse = nums.reverse();
+        reverse.print();
     }
 }
