2a4714e6d210cd51fcfc2e83e4b13971c3a83fe0
OldTrace <liyedadi@gmail.com>
2025-05-09 20:45:47 +0800

    5月9日每日一题：直接放弃

---
+++ src/CountBalancedPermutations.java
@@ -0,0 +1,19 @@
+public class CountBalancedPermutations {
+    /*
+        题目: 3343. 统计平衡排列的数目
+        链接: https://leetcode.cn/problems/count-number-of-balanced-permutations/description/?envType=daily-question&envId=2025-05-09
+        难度: 困难
+        思路: 给你一个字符串num 。如果一个数字字符串的奇数位下标的数字之和与偶数位下标的数字之和相等，那么我们称这个数字字符串是平衡的 。
+        时间: 2025年5月9日20:40:31
+        思路：没有思路，直接题解。嗯，题解是公式推导，放弃了。
+    */
+
+    public static void main(String[] args) {
+        CountBalancedPermutations main = new CountBalancedPermutations();
+        System.out.println(main);
+    }
+
+    public int countBalancedPermutations(String num) {
+        return 0;
+    }
+}
